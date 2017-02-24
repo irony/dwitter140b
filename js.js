f577dd93facc4ceb62c9adbf9016c032cc436b01
Christian Landgren <christian@landgren.nu>
2017-02-24 10:33:51 +0100

    added proof and added comments. All file sizes are now 140b

--- js.js
+++ js.js
@@ -1,2 +1,3 @@
-c.width=1920
-for(i=9;i;i--) c.getContext('2d').fillRect(200+i*100+Math.sin(f/120)*300,0,50,100)
+// f = current frame
+c.width=1920 // clear canvas (c)
+for(i=0; i<9; i++) c.getContext('2d').fillRect(200+i*100+Math.sin(f/120)*300,0,50,100)
---
+++ proof.md
@@ -0,0 +1,7 @@
+
+140B Feb 24 10:21 README.md
+140B Feb 24 10:13 editor.html
+140B Feb 24 10:17 index.html
+140B Feb 24 10:14 js.js
+140B Feb 24 10:33 proof.md
+
